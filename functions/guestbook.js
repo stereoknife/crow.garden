4f77380dd1fa3e89d0665401f5a6c05e2e275d56
IG <11618271+stereoknife@users.noreply.github.com>
2024-11-28 19:43:26 +0100

    cant figure this out

--- functions/guestbook.js
+++ functions/guestbook.js
@@ -5,7 +5,8 @@ export async function onRequestGet(context) {
 }
 
 export async function onRequestPost(context){
-	const body = JSON.parse(context.request.body)
+	const body = context.request.body
+	console.log(body)
 	const query = context.env.CROW_DB
 		.prepare('INSERT INTO guestbook VALUES(?, ?, ?)')
 		.bind(body.name, body.comment, body.site)
