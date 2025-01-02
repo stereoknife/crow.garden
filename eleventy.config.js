feada9c30e3ede2cce025683ba97a71e234e12ac
IG <11618271+stereoknife@users.noreply.github.com>
2025-01-02 11:12:42 +0100

    Tadi web ✔️

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,7 @@ import pluginWebc from "@11ty/eleventy-plugin-webc";
 
 export default function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebc, {
-		components: "src/_components/**/*.webc",
+		components: "src/_includes/components/**/*.webc",
 	});
 	eleventyConfig.addPassthroughCopy("public");
 	eleventyConfig.setInputDirectory("src");
